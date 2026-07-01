43a1566f3edfa1791c917801f5999e1840394a45
Tiago Lima <lima_tiago@hotmail.com>
2026-07-01 19:27:07 +0200

    feat(profile): add edit name form with save and cancel actions

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,17 +1,72 @@
-import { useSelector } from "react-redux";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { updateUserProfile } from "../services/apiService";
+import { fetchUserProfile } from "../redux/slices/authSlice";
 
 function Profile() {
-  const { user } = useSelector((state) => state.auth);
+  const { user, token } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const [isEditing, setIsEditing] = useState(false);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const handleEditClick = () => {
+    setFirstName(user?.firstName || "");
+    setLastName(user?.lastName || "");
+    setIsEditing(true);
+  };
+
+  const handleSave = async () => {
+    await updateUserProfile(token, { firstName, lastName });
+    dispatch(fetchUserProfile(token));
+    setIsEditing(false);
+  };
+
+  const handleCancel = () => {
+    setIsEditing(false);
+  };
 
   return (
     <main className="main bg-light">
       <div className="header">
-        <h1>
-          Welcome back
-          <br />
-          {user?.firstName} {user?.lastName}!
-        </h1>
-        <button className="edit-button">Edit Name</button>
+        {isEditing ? (
+          <>
+            <h1>Welcome back</h1>
+            <div className="edit-form">
+              <div className="edit-form-inputs">
+                <input
+                  type="text"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+                <input
+                  type="text"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+              </div>
+              <div className="edit-form-buttons">
+                <button className="edit-save-button" onClick={handleSave}>
+                  Save
+                </button>
+                <button className="edit-cancel-button" onClick={handleCancel}>
+                  Cancel
+                </button>
+              </div>
+            </div>
+          </>
+        ) : (
+          <>
+            <h1>
+              Welcome back
+              <br />
+              {user?.firstName} {user?.lastName}!
+            </h1>
+            <button className="edit-button" onClick={handleEditClick}>
+              Edit Name
+            </button>
+          </>
+        )}
       </div>
       <h2 className="sr-only">Accounts</h2>
       <section className="account">
