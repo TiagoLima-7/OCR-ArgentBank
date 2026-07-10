071ea1b362fc844446ba3448206a3a2be2fafc7d
Tiago Lima <lima_tiago@hotmail.com>
2026-07-10 10:30:18 +0200

    feat(config): add environment variables for API URL.

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3001/api/v1";
-
 const apiClient = axios.create({
-  baseURL: API_URL,
+  baseURL: import.meta.env.VITE_API_URL, //Using .env variable
 });
 
 //Login - token recovery
